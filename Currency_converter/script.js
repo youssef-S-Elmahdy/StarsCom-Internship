cae04bc331b2e2a9482e141c951270909a8c70e4
Youssef_Elmahdy <yelmahdy@aucegypt.edu>
2023-08-20 00:50:10 +0300

    finished9th project

---
+++ Currency_converter/script.js
@@ -0,0 +1,23 @@
+const currencyFirstEl = document.getElementById("currency-first");
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate();
+
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/62e53b90cf6624ae25e35ed6/latest/${currencyFirstEl.value}`).then((res)=>res.json()).then((data)=>{
+        const rate = data.conversion_rates[currencySecondEl.value];
+        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(3);
+})
+}
+
+
+currencyFirstEl.addEventListener("change", updateRate);
+currencySecondEl.addEventListener("change", updateRate);
+worthFirstEl.addEventListener("input", updateRate);
+
